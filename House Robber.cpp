cd6f9269512c3106a699de10880e93f469b48082
ArronWung <847721341@qq.com>
2015-04-06 22:34:06 +0800

    Update House Robber.cpp

--- House Robber.cpp
+++ House Robber.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int rob(vector<int> &num) {
-        int n;
         
-        if(num.size() == 0)
+        if (num.size() == 0)
             return 0;
-      //  if(num.size() == 1)
-        //    return num[0];
-        if(num.size() > 1 && num[0] > num[1])
+        if (num.size() > 1 && num[0] > num[1])
             num[1] = num[0];
-        for(n = 2; n < num.size(); n++)
+        for (int n = 2; n < num.size(); n++)
             num[n] = (num[n] + num[n-2] > num[n-1] ? num[n] + num[n-2] : num[n-1]);
         return num[num.size()-1];
     }
